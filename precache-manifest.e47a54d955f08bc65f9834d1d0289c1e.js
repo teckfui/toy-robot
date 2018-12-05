52a3b752d04e080073a5ed90d11929cbd1dec8a6
Teck Fui Yong <teckfui@gmail.com>
2018-12-06 02:41:12 +1100

    Updates

---
+++ precache-manifest.e47a54d955f08bc65f9834d1d0289c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e128f9c15661f67fa43",
+    "url": "/toy-robot/static/css/main.bd60fe40.chunk.css"
+  },
+  {
+    "revision": "3e128f9c15661f67fa43",
+    "url": "/toy-robot/static/js/main.3e128f9c.chunk.js"
+  },
+  {
+    "revision": "4e68c649dc659d606f0c",
+    "url": "/toy-robot/static/js/1.4e68c649.chunk.js"
+  },
+  {
+    "revision": "21c9940cdca758ed6d36",
+    "url": "/toy-robot/static/js/runtime~main.21c9940c.js"
+  },
+  {
+    "revision": "5717bc2c3cfa1e38b509776d4b513590",
+    "url": "/toy-robot/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54346ade0c0737a23bd7e1cf664c1d2b.js"
+  "/toy-robot/precache-manifest.e47a54d955f08bc65f9834d1d0289c1e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/toy-robot/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
