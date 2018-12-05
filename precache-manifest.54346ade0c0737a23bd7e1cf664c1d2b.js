e027b1899fee9637b58620707e150d8c782a886d
Teck Fui Yong <teckfui@gmail.com>
2018-12-06 02:40:22 +1100

    Updates

---
+++ precache-manifest.54346ade0c0737a23bd7e1cf664c1d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e128f9c15661f67fa43",
+    "url": "/static/css/main.bd60fe40.chunk.css"
+  },
+  {
+    "revision": "3e128f9c15661f67fa43",
+    "url": "/static/js/main.3e128f9c.chunk.js"
+  },
+  {
+    "revision": "4e68c649dc659d606f0c",
+    "url": "/static/js/1.4e68c649.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99e15e6fddf97700ed7b4d2100931611",
+    "url": "/index.html"
+  }
+];
